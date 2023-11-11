af31f4dd8877acc2b9ebd8a47cda8728f90e607d
electr0n4ik <andreyshka3@gmail.com>
2023-11-11 12:33:29 +0300

    upd

--- realtimeplot2.py
+++ realtimeplot2.py
@@ -2,30 +2,27 @@ import asyncio
 import json
 import time
 
-import asyncpg
 import pandas as pd
 import websockets
+from sqlalchemy import create_engine, Column, Integer, String, Numeric, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Trade(Base):
+    __tablename__ = 'trades'
+
+    id = Column(Integer, primary_key=True)
+    symbol = Column(String)
+    price = Column(Numeric)
+    timestamp = Column(DateTime)
 
 
 async def create_table():
-    conn = await asyncpg.connect(
-        user='postgres',
-        password='12345',
-        database='postgres',
-        port='5432'
-    )
-
-    # Создание таблицы, если она не существует
-    await conn.execute('''
-        CREATE TABLE IF NOT EXISTS trades (
-            id serial PRIMARY KEY,
-            symbol TEXT,
-            price NUMERIC,
-            timestamp TIMESTAMP
-        )
-    ''')
-
-    await conn.close()
+    engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+    Base.metadata.create_all(engine)
 
 
 async def handle_trade(trade):
@@ -34,52 +31,40 @@ async def handle_trade(trade):
     symbol = data['s']
     price = data['p']
 
-    conn = await asyncpg.connect(
-        user='postgres',
-        password='12345',
-        database='postgres',
-        port='5432'
-    )
+    engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     # Вставка данных в таблицу
-    await conn.execute('''
-        INSERT INTO trades (symbol, price, timestamp) VALUES ($1, $2, NOW())
-    ''', symbol, price)
-
-    await conn.close()
+    trade_entry = Trade(symbol=symbol, price=price, timestamp=time.strftime('%Y-%m-%d %H:%M:%S'))
+    session.add(trade_entry)
+    session.commit()
+    session.close()
 
     # пауза в 10 секунд
     await asyncio.sleep(10)
 
 
 async def delete_old_data():
-    conn = await asyncpg.connect(
-        user='postgres',
-        password='12345',
-        database='postgres',
-        port='5432'
-    )
+    engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     # Удаление данных старше 1 часа
-    await conn.execute('''
-        DELETE FROM trades WHERE timestamp < NOW() - interval '1 hour'
-    ''')
-
-    await conn.close()
+    session.query(Trade).filter(
+        Trade.timestamp < time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() - 3600))).delete()
+    session.commit()
+    session.close()
 
 
 async def calculate_correlation():
-    conn = await asyncpg.connect(
-        user='postgres',
-        password='12345',
-        database='postgres',
-        port='5432'
-    )
+    engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     try:
         # SQL-запрос для извлечения данных из базы данных
-        query = 'SELECT timestamp, price FROM trades ORDER BY timestamp'
-        result = await conn.fetch(query)
+        result = session.query(Trade.timestamp, Trade.price).order_by(Trade.timestamp).all()
 
         # DataFrame из полученных данных
         data = pd.DataFrame(result, columns=['X', 'Y'])
@@ -94,18 +79,23 @@ async def calculate_correlation():
                 print("Недостаточно данных для расчета корреляции.")
         else:
             print("Недостаточно данных для расчета корреляции.")
-    except asyncpg.PostgresError as e:
+    except Exception as e:
         print(f"Error executing query: {e}")
     finally:
-        await conn.close()
+        session.close()
 
 
 async def price_change_alert(data, price_change_threshold=0.01, time_frame=60):
+    engine = create_engine('postgresql://postgres:12345@localhost:5432/postgres')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     price_history = []
 
     while True:
         current_price = data["p"]
         timestamp = time.time()
+
         price_history.append((current_price, timestamp))
 
         # Оставляем только данные, которые находятся в заданном временном диапазоне
@@ -126,6 +116,8 @@ async def price_change_alert(data, price_change_threshold=0.01, time_frame=60):
                 print(f"Изменение цены на {abs(percent_change) * 100:.2f}% за последние {time_frame} минут. {movement}")
 
         await asyncio.sleep(10)  # Проверяем каждые 10 секунд
+
+    session.close()
 
 
 async def main():
